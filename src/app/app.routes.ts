1a415408b91354c9bd0827493dd00670cda74e93
Marco <marco1102004@gmail.com>
2024-03-08 18:16:49 -0600

    añadiendo variables de entorno

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './Layouts/auth/auth.component';
+import { LoginComponent } from './Views/Auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AuthComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      }
+    ]
+  }
+];
